353c78cc11b03280219cf46ff25be44ed50d133c
Roman_Pidgurskiy <romaliviv@gmail.com>
2019-04-11 23:56:51 +0300

    added first project to github

---
+++ Lesson_5_2/src/ua/lviv/lgs/Application.java
@@ -0,0 +1,14 @@
+package ua.lviv.lgs;
+
+public class Application {
+	
+
+	public static void main(String[] args) {
+		
+		 int x = (int)(Math.random()*((1-0)+1))+0;
+		 if(x==1)System.out.println("RESHKA");
+		  else
+		   System.out.println("OREL");	
+	}
+
+}
